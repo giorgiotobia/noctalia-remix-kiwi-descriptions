ab91c656323e2e62acdbb068f3d050dcb2bcc7c9
giorgio.tobia <giorgio.tobia@gmail.com>
2026-08-30 23:21:29 +0200

    duplicated lines deleted

--- config.sh
+++ config.sh
@@ -448,28 +448,6 @@ GREETER_USER=greetd /usr/share/noctalia-greeter/setup_greeter_system.sh
 mv /tmp/additional_setup.sh /usr/local/bin
 chmod +x /usr/local/bin/additional_setup.sh
 # Enable greetd service
-systemctl enable greetd.service
-
-fi
-
-
-if [[ "$kiwi_profiles" == *"Noctalia"* ]]; then
-# enable noctalia-greeter in greetd config
-cat <<EOF > /etc/greetd/config.toml
-[terminal]
-vt = 1
-
-[default_session]
-command = "/usr/bin/noctalia-greeter-session"
-user = "greetd"
-EOF
-
-# Setup noctalia-greeter
-GREETER_USER=greetd /usr/share/noctalia-greeter/setup_greeter_system.sh
-# Copy postinstall script
-mv /tmp/additional_setup.sh /usr/local/bin
-chmod +x /usr/local/bin/additional_setup.sh
-# Enable greetd service
 systemctl enable livesys.service
 systemctl enable livesys-late.service
 systemctl enable greetd.service
