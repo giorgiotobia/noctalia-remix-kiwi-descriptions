889a5d00f23cfd96d386b466e551cf79c11653a4
giorgio.tobia <giorgio.tobia@gmail.com>
2026-09-09 10:47:11 +0200

    outputs.toml in first user config

--- root/tmp/additional_setup.sh
+++ root/tmp/additional_setup.sh
@@ -13,9 +13,12 @@ fi
 LC_NUMERIC="en_US.UTF-8" printf "\n[output.\"%s\"]\nmode = \"%s@%.0f\"\nscale = %s\n" $MONITOR $RESOLUTION $FREQUENCY $SCALE
 done < <(umbriel outputs | awk '{if($0~/^[A-Z][A-Za-z]*-[0-9]/) mon=$1; {if($0~/current/) print mon, $1, $3}}'))
 
-echo "$UMBRIEL_OUTPUTS" >> /home/liveuser/.config/umbriel/config.toml
+echo "$UMBRIEL_OUTPUTS" > /home/liveuser/.config/umbriel/outputs.toml
 
+# Delete comments after files list, otherwise the script that apply theme for umbriel doesn't work
 sed -i -r -e "s/(files = \[.*\]) *#.*$/\1/" /home/liveuser/.config/umbriel/config.toml
+# add current output to liveuser configuration
+sed -i -r -e "/\[include\]$/,/^$/ s/(files = \[)\]/\1\"outputs.toml\"\]/" /home/liveuser/.config/umbriel/config.toml
 
 IS_VIRTUAL=$(systemd-detect-virt)
 
@@ -50,18 +53,20 @@ set -a
 source /mnt/sysroot/etc/vconsole.conf
 sudo bash -c 'sed -i -r -e "/\[input.keyboard\]/,/^$/ s/^(layout = \").*/\1'$KEYMAP'\"/" -e "/(files = \[.*\]) *#.*$/\1/" /mnt/sysroot/etc/skel/.config/'${WM}'/config.toml'
 set +a
-sudo bash -c 'echo '"$UMBRIEL_OUTPUTS"' >> /mnt/sysroot/etc/skel/.config/'${WM}'/config.toml'
 fi
 
 if [ -f /mnt/sysroot/etc/xdg/foot/foot.ini ]; then
 sudo cp -r /mnt/sysroot/etc/xdg/foot /mnt/sysroot/etc/skel/.config/
-# sudo bash -c 'sed -i -r -e "s/^.*(pad=)[0-9]*x[0-9]*(.*)/\15x5\2/" /mnt/sysroot/etc/skel/.config/foot/foot.ini'
 fi
 
 FIRSTHOME=$(find /mnt/sysroot/home -maxdepth 1 -mindepth 1 -type d)
 if [ -n "$FIRSTHOME" ]; then
 sudo cp -r /mnt/sysroot/etc/skel/.config/ $FIRSTHOME
 sudo chown -R $(stat -c %u:%g $FIRSTHOME) $FIRSTHOME/.config/
+# add current output to first user configuration
+sudo bash -c 'echo '"$UMBRIEL_OUTPUTS"' > '$FIRSTHOME'/.config/'${WM}'/outputs.toml'
+sudo bash -c 'sed -i -r -e "/\[include\]$/,/^$/ s/(files = \[)\]/\1\"outputs.toml\"\]/" '$FIRSTHOME'/.config/'${WM}'/config.toml'
+sudo bash -c 'sed -i -r -e "s/^.*(pad=)[0-9]*x[0-9]*(.*)/\15x5\2/" '$FIRSTHOME'/.config/foot/foot.ini'
 fi
 
 sudo rm -f /mnt/sysroot/usr/local/bin/additional_setup.sh
